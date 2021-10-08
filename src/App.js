039ebf67f05e301405c705d3e1e839daffef7eae
mukitur <mukit.inway@gmail.com>
2021-10-08 16:27:43 +0600

    process start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { useState } from "react";
 import './App.css';
+import initializeAuthentication from './Firebase/firebase.init';
+
+initializeAuthentication();
+const googleProvider = new GoogleAuthProvider();
 
 function App() {
+  const [user, setUser] =useState({});
+  const auth = getAuth();
+
+  const handleGoogleSignIn = () =>{
+
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const { displayName, email, photoURL } = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(loggedInUser);
+      })
+      .catch(error => {
+        console.log(error.message);
+      })
+  }
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> Welcome to our site</h2>
+      <button onClick ={handleGoogleSignIn}>Google Sign In</button>
+      <br/>
+      <button onClick={handleSignOut}>Sign Out</button>
+      
+      {
+        user.name && <div>
+        <h2>Welcome {user.name}</h2>
+        <p>Your Email Address is: {user.email}</p>
+        <img src={user.photo} alt="" />
+      </div>
+      }
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBW84LDmZ9uCWrxvHgtbmHOU0E22zYLWxA",
+  authDomain: "react-firebase-signin-process.firebaseapp.com",
+  projectId: "react-firebase-signin-process",
+  storageBucket: "react-firebase-signin-process.appspot.com",
+  messagingSenderId: "796656058988",
+  appId: "1:796656058988:web:e33f6d4aa80af8a71e4e49"
+};
+
+  export default firebaseConfig;
